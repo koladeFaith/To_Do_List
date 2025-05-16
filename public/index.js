a3bb7024e0cab609e4b952d1e940b19b2e971425
Kolade Faith <koladefaith1000@gmail.com>
2025-05-16 13:19:01 +0100

    Sign In page added

---
+++ public/index.js
@@ -0,0 +1,15 @@
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDAoVD28HZJSvNYxJnZ76SQz76c6phyy60",
+    authDomain: "class-d93aa.firebaseapp.com",
+    projectId: "class-d93aa",
+    storageBucket: "class-d93aa.firebasestorage.app",
+    messagingSenderId: "416215641552",
+    appId: "1:416215641552:web:4cc22bb840dbfcf92e5dd5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
