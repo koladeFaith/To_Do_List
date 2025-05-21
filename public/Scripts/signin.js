29310931726f521283433be134227380a04787cf
Kolade Faith <koladefaith1000@gmail.com>
2025-05-21 13:44:41 +0100

    Work done

--- public/Scripts/signin.js
+++ public/Scripts/signin.js
@@ -81,3 +81,16 @@ const signInUser = () => {
     }
 }
 window.signInUser = signInUser;
+
+const password = document.getElementById('pass')
+const show = document.querySelector("#show")
+show.addEventListener("click", () => {
+    if (password.getAttribute("type") === "password") {
+        password.setAttribute("type", "text")
+        show.classList.replace("bi-eye", "bi-eye-slash")
+    } else {
+        password.setAttribute("type", "password")
+        show.classList.replace("bi-eye-slash", "bi-eye")
+
+    }
+})
